12e3615555e9047edd94df35ffb047035db41758
tnicolas28 <54882586+tnicolas28@users.noreply.github.com>
2022-03-07 00:59:29 +0100

    Retrieve data from form

--- iForm/iForm.swift
+++ iForm/iForm.swift
@@ -12,13 +12,15 @@ public typealias Action = ()->()
 
 @available(iOS 14.0, *)
 public class iForm {
+    
     public init() {}
     
     func buttonClicked() {
         print("button clicked")
     }
     
-    public func addSignInForm(parentView : UIView) -> UIView {
+    public func addSignInForm(parentView : UIView, actionToPerform : UIAction) -> UIView {
+        
         let view = UIView(frame: CGRect(x: 0, y: 0, width: parentView.frame.size.width, height: parentView.frame.size.height))
         
         let emailTextField = iFormTextFieldItem(placeholder: "Email")
@@ -29,9 +31,7 @@ public class iForm {
         passwordTextField.setConstraints(Constraints(horizontal: 0, vertical: 50, width: 200, height: 30))
         passwordTextField.setBackgroundColor(.white)
         
-        let loginButton = iFormButtonItem(text: "Sign in", textColor: .darkGray, backgroundColor: .lightGray, action: UIAction() { _ in
-            self.buttonClicked()
-        })
+        let loginButton = iFormButtonItem(text: "Sign in", textColor: .darkGray, backgroundColor: .lightGray, action: actionToPerform)
         
         loginButton.setConstraints(Constraints(horizontal: 0, vertical: 150, width: 200, height: 30))
         
@@ -41,6 +41,31 @@ public class iForm {
         
         return view
         
+    }
+    
+    public func getTextFieldsValues(values : [UITextField]) -> [String:String] {
+        var textfieldsValues = [String:String]()
+        
+        for txtValue in values {
+            if let placeHolder = txtValue.placeholder {
+                if(!placeHolder.isEmpty){
+                    textfieldsValues[placeHolder] = txtValue.text
+                }
+            }
+        }
+        
+        
+        return textfieldsValues
+    }
+    
+    public func getAllTextFields(from view : UIView) -> [UITextField] {
+        view.subviews.compactMap{ (view) -> [UITextField]? in
+            if view is UITextField {
+                return [(view as! UITextField)]
+            } else {
+                return getAllTextFields(from: view)
+            }
+        }.flatMap({$0})
     }
     
 }
