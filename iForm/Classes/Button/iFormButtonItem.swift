0ede969d40ac8aa70ced41ae003434c95c5ee798
Gwendal Vallée <gwendal.vallee@hotmail.fr>
2022-03-07 11:54:59 +0100

    set iFormTextField as UITextField

--- iForm/Classes/Button/iFormButtonItem.swift
+++ iForm/Classes/Button/iFormButtonItem.swift
@@ -8,12 +8,13 @@
 import Foundation
 import UIKit
 
+@available(iOS 14.0, *)
 public class iFormButtonItem: iFormDelegate {
     //from iFormDelegate
-    public var text: String?
-    public var textColor: UIColor?
-    public var backgroundColor: UIColor?
-    public var constraints: Constraints?
+    public private(set) var txt: String?
+    public private(set) var txtColor: UIColor?
+    public private(set) var bcgColor: UIColor?
+    public private(set) var constr: Constraints?
     
     private var button: UIButton = UIButton()
     @objc private var action: UIAction
@@ -24,59 +25,61 @@ public class iFormButtonItem: iFormDelegate {
         backgroundColor: UIColor? = UIColor.lightGray,
         constraints: Constraints? = Constraints(horizontal: 0, vertical: 0, width: 100, height: 15),
         action: UIAction) {
-            self.text            = text
-            self.textColor       = textColor
-            self.backgroundColor = backgroundColor
-            self.constraints     = constraints
+            self.txt            = text
+            self.txtColor       = textColor
+            self.bcgColor        = backgroundColor
+            self.constr          = constraints
             self.action          = action
+            
     }
+    
     
     @available(iOS 14.0, *)
     public func display(on view: UIView) {
         self.button = UIButton(type : .system,primaryAction: self.action)
         
-        self.button.setTitle(self.text, for: .normal)
+        self.button.setTitle(self.txt, for: .normal)
         
         //Styling button's layer
         self.button.layer.cornerRadius  = CGFloat(5.0)
         self.button.layer.borderColor   = UIColor.lightGray.cgColor
         self.button.layer.borderWidth   = CGFloat(1.0)
         
-        self.button.backgroundColor     = self.backgroundColor
+        self.button.backgroundColor     = self.bcgColor
         
-        self.button.setTitle(self.text, for: .normal)
-        self.button.setTitleColor(self.textColor, for: .normal)
+        self.button.setTitle(self.txt, for: .normal)
+        self.button.setTitleColor(self.txtColor, for: .normal)
         
-        self.button.frame               = CGRect(x: (constraints!.getHorizontal()), y: constraints!.getVertical(), width: constraints!.getWidth()!,height: constraints!.getHeight()!)
+        self.button.frame               = CGRect(x: (constr!.getHorizontal()), y: constr!.getVertical(), width: constr!.getWidth()!,height: constr!.getHeight()!)
         
 
         view.addSubview(self.button)
         self.button.translatesAutoresizingMaskIntoConstraints = false
         
-        let horizontalConstraint = NSLayoutConstraint(item: self.button, attribute: NSLayoutConstraint.Attribute.centerX, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view, attribute: NSLayoutConstraint.Attribute.centerX, multiplier: 1, constant: self.constraints!.getHorizontal())
-        let verticalConstraint = NSLayoutConstraint(item: self.button, attribute: NSLayoutConstraint.Attribute.centerY, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view, attribute: NSLayoutConstraint.Attribute.centerY, multiplier: 1, constant: self.constraints!.getVertical())
-        let widthConstraint = NSLayoutConstraint(item: self.button, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: self.constraints!.getWidth()!)
-        let heightConstraint = NSLayoutConstraint(item: self.button, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: self.constraints!.getHeight()!)
+        let horizontalConstraint = NSLayoutConstraint(item: self.button, attribute: NSLayoutConstraint.Attribute.centerX, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view, attribute: NSLayoutConstraint.Attribute.centerX, multiplier: 1, constant: self.constr!.getHorizontal())
+        let verticalConstraint = NSLayoutConstraint(item: self.button, attribute: NSLayoutConstraint.Attribute.centerY, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view, attribute: NSLayoutConstraint.Attribute.centerY, multiplier: 1, constant: self.constr!.getVertical())
+        let widthConstraint = NSLayoutConstraint(item: self.button, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: self.constr!.getWidth()!)
+        let heightConstraint = NSLayoutConstraint(item: self.button, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: self.constr!.getHeight()!)
         
         view.addConstraints([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
            
     }
         
     public func setText(_ text: String) {
-        self.text = text
+        self.txt = text
     }
     
     public func setTextColor(_ color: UIColor) {
-        self.textColor = color
+        self.txtColor = color
     }
     
     public func setBackgroundColor(_ color: UIColor) {
-        self.backgroundColor = color
+        self.bcgColor = color
     }
     
     
     public func setConstraints(_ constraints: Constraints) {
-        self.constraints = constraints
+        self.constr = constraints
     }
     
     public func setBorderRadius(radius : CGFloat){
--- iForm/iForm.swift
+++ iForm/iForm.swift
@@ -15,33 +15,59 @@ public class iForm {
     
     public init() {}
     
-    func buttonClicked() {
-        print("button clicked")
-    }
-    
-    public func addSignInForm(parentView : UIView, actionToPerform : UIAction) -> UIView {
+    public func initSignInForm(on parentView : UIView, actionToPerform : UIAction) -> UIView {
         
         let view = UIView(frame: CGRect(x: 0, y: 0, width: parentView.frame.size.width, height: parentView.frame.size.height))
         
-        let emailTextField = iFormTextFieldItem(placeholder: "Email")
-        emailTextField.setConstraints(Constraints(horizontal: 0, vertical: 0, width: 200, height: 30))
-        emailTextField.setBackgroundColor(.white)
+        let emailTextField = self.initTextField(
+            placeholder: "Email",
+            backgroundColor: .white,
+            constraints: Constraints(horizontal: 0, vertical: 0, width: 200, height: 30)
+        )
 
-        let passwordTextField = iFormTextFieldItem(placeholder: "Password")
-        passwordTextField.setConstraints(Constraints(horizontal: 0, vertical: 50, width: 200, height: 30))
-        passwordTextField.setBackgroundColor(.white)
+        let passwordTextField = self.initTextField(
+            placeholder: "Mot de passe",
+            backgroundColor: .white,
+            constraints: Constraints(horizontal: 0, vertical: 50, width: 200, height: 30)
+        )
         
-        let loginButton = iFormButtonItem(text: "Sign in", textColor: .darkGray, backgroundColor: .lightGray, action: actionToPerform)
-        
-        loginButton.setConstraints(Constraints(horizontal: 0, vertical: 150, width: 200, height: 30))
+        let loginButton = iFormButtonItem(
+            text: "Se connecter",
+            textColor: .darkGray,
+            backgroundColor: .lightGray,
+            constraints: Constraints(horizontal: 0, vertical: 150, width: 200, height: 30),
+            action: actionToPerform
+        )
         
         emailTextField.display(on: view)
         passwordTextField.display(on: view)
         loginButton.display(on: view)
         
-    
         return view
         
+    }
+    
+    
+    public func initTextField(
+        placeholder: String? = "",
+        text: String? = "",
+        textColor: UIColor? = .darkText,
+        backgroundColor: UIColor? = .lightGray,
+        constraints: Constraints? = Constraints(horizontal: 0, vertical: 30, width: 200, height: 30),
+        contentType: iFormTextFieldContentType? = .normal,
+        keyboardType: iFormTextFieldKeyboardType? = .normal
+    ) -> iFormTextFieldItem {
+        return iFormTextFieldItem(frame: CGRect(x: constraints!.getHorizontal(), y: constraints!.getVertical(), width: constraints!.getWidth()!, height: constraints!.getHeight()!), placeholder: placeholder, text: text, textColor: textColor, backgroundColor: backgroundColor, constraints: constraints, contentType: contentType, keyboardType: keyboardType)
+    }
+    
+    public func initButton(
+        text: String? = "",
+        textColor: UIColor? = .darkText,
+        backgroundColor: UIColor? = .lightGray,
+        constraints: Constraints? = Constraints(horizontal: 0, vertical: 40, width: 200, height: 50),
+        action: UIAction
+    ) -> iFormButtonItem {
+        return iFormButtonItem(text: text, textColor: textColor, backgroundColor: backgroundColor, constraints: constraints, action: action)
     }
     
     public func getTextFieldsValues(values : [UITextField]) -> [String:String] {
@@ -69,30 +95,5 @@ public class iForm {
         }.flatMap({$0})
     }
     
-    
-    public func initTextField(
-        placeholder: String? = "",
-        text: String? = "",
-        textColor: UIColor? = .darkText,
-        backgroundColor: UIColor? = .lightGray,
-        constraints: Constraints? = Constraints(horizontal: 0, vertical: 0, width: 200, height: 20),
-        contentType: iFormTextFieldContentType? = .normal,
-        keyboardType: iFormTextFieldKeyboardType? = .normal
-    ) -> UIView {
-        let textField = iFormTextFieldItem(placeholder: placeholder, text: text, textColor: textColor, backgroundColor: backgroundColor, constraints: constraints, contentType: contentType, keyboardType: keyboardType)
-        let view = UIView(frame: CGRect(x: textField.constraints!.getVertical(), y: textField.constraints!.getHorizontal(), width: textField.constraints!.getWidth()!, height: textField.constraints!.getHeight()!))
-       
-        return view
-    }
-    
-    public func initButton(
-        text: String? = "",
-        textColor: UIColor? = .darkText,
-        backgroundColor: UIColor? = .lightGray,
-        constraints: Constraints? = Constraints(horizontal: 0, vertical: 0, width: 200, height: 50),
-        action: UIAction
-    ) -> iFormButtonItem {
-        return iFormButtonItem(text: text, textColor: textColor, backgroundColor: backgroundColor, constraints: constraints, action: action)
-    }
     
 }
